82308fe3beb3cbcf98cc67e454cde520d2b4c29c
allan-schj <46282604+allan-schj@users.noreply.github.com>
2019-02-17 15:15:55 +0100

    Add files via upload

---
+++ precache-manifest.509671c4dd90e790618a3a0370129d54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b419ccb7ad5539636cb6",
+    "url": "/static/js/main.b419ccb7.chunk.js"
+  },
+  {
+    "revision": "e865db3d9ca0c72e4d5f",
+    "url": "/static/js/1.e865db3d.chunk.js"
+  },
+  {
+    "revision": "b419ccb7ad5539636cb6",
+    "url": "/static/css/main.b44ba854.chunk.css"
+  },
+  {
+    "revision": "a4ec44cb4dac75232d3747ada8afc7e2",
+    "url": "/index.html"
+  }
+];
